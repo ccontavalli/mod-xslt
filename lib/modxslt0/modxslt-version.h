280a53812d9f3b7776fef8d108438ff3e1b3578f
Carlo Contavalli <ccontavalli@inscatolati.net>
2011-09-17 16:35:35 -0700

    Updated version number, and updated to use latest versions of autotools.

--- lib/modxslt0/modxslt-version.h
+++ lib/modxslt0/modxslt-version.h
@@ -1,4 +1,4 @@
-#define MXSLT_VERSION "1.3.9"
+#define MXSLT_VERSION "1.4.0"
 #define MXSLT_MAJOR "1"
-#define MXSLT_MINOR "3"
-#define MXSLT_PATCHLEVEL "9"
+#define MXSLT_MINOR "4"
+#define MXSLT_PATCHLEVEL "0"
